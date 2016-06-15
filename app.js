ced0e9443fc106501175df6daffa941c158ac1ff
gmwishwas <wishwas.mohan@entytle.com>
2016-06-15 15:55:45 -0700

    added port

--- app.js
+++ app.js
@@ -154,5 +154,7 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 4004, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
